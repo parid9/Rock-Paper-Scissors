cba912a3073405a525f201e38423fbeeafa91958
Parid9 <138480015+parid9@users.noreply.github.com>
2023-08-01 14:18:11 +0200

    Added way for the first box to be clickable

---
+++ app.js
@@ -0,0 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const box1 = document.querySelector('#box1');
+box1.addEventListener('click', () => {
+  game1();
+});
+function game1() {
+  console.log('hello');
+}
