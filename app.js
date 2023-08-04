426b4270e59668d5a921fde61da8ad08b8aa8cb0
Parid9 <138480015+parid9@users.noreply.github.com>
2023-08-04 13:53:53 +0200

    Added winning, losing and drawing conditions

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const choices = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 let playerChoice;
-
+let playing = true;
 // Box Elements
 
 const box1 = document.querySelector('#box1');
 const box3 = document.querySelector('#box3');
 const box2 = document.querySelector('#box2');
+
+// Other Elements
+
+const message = document.querySelector('message');
 
 // Boxes Functionality
 
@@ -36,8 +40,33 @@ function generateComputerChoice() {
 
 function game() {
   const computerChoice = generateComputerChoice();
-  console.log(computerChoice);
 
   if (playerChoice === computerChoice) {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You tied!';
+  }
+  if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You won|Rock beats scissors.';
+  }
+  if (playerChoice === 'paper' && computerChoice === 'rock') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You won|Paper beats rock.';
+  }
+  if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You won|Scissors beats paper.';
+  }
+  if (playerChoice === 'rock' && computerChoice === 'paper') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You lost|Paper beats rock.';
+  }
+  if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You lost|Scissors beats paper.';
+  }
+  if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    let outCome = document.getElementById('message');
+    outCome.textContent = 'You lost|Rock beats scissors.';
   }
 }
