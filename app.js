9882168d7bced820b1438ae4490200a5d79e63ca
Parid9 <138480015+parid9@users.noreply.github.com>
2023-08-04 12:56:10 +0200

    Fixed a bug

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-let playerScore = 0;
-let computerScore = 0;
+// Choice Array
 
 const choices = ['rock', 'paper', 'scissors'];
 
+// Game Variables
+
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+
+// Box Elements
+
 const box1 = document.querySelector('#box1');
-
-box1.addEventListener('click', () => {
-  game();
-});
-
+const box3 = document.querySelector('#box3');
 const box2 = document.querySelector('#box2');
 
-box2.addEventListener('click', () => {
+// Boxes Functionality
+
+box1.addEventListener('click', () => {
+  playerChoice = 'rock';
   game();
 });
 
-const box3 = document.querySelector('#box3');
+box2.addEventListener('click', () => {
+  playerChoice = 'paper';
+  game();
+});
 
 box3.addEventListener('click', () => {
+  playerChoice = 'scissors';
   game();
 });
-
-function game() {
-  console.log('hello');
-}
-
 function generateComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(generateComputerChoice());
+function game() {
+  const computerChoice = generateComputerChoice();
+  console.log(computerChoice);
+
+  if (playerChoice === computerChoice) {
+  }
+}
