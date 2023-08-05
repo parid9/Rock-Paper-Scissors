10a9a3ec36a70d14bce545cbc0501ba3e64befa7
Parid9 <138480015+parid9@users.noreply.github.com>
2023-08-05 14:16:16 +0200

    Added a way to increment points and make the game stop if someone reaches a score of 5

--- app.js
+++ app.js
@@ -41,32 +41,55 @@ function generateComputerChoice() {
 function game() {
   const computerChoice = generateComputerChoice();
 
+  if (playing !== true) {
+    return;
+  }
+  if (playerScore === 5) {
+    playing = false;
+  }
+  if (computerScore === 5) {
+    playing = false;
+  }
   if (playerChoice === computerChoice) {
     let outCome = document.getElementById('message');
-    outCome.textContent = 'You tied!';
+    outCome.textContent = `You tied! Player choice: ${playerChoice}|Computer choice: ${computerChoice}`;
   }
   if (playerChoice === 'rock' && computerChoice === 'scissors') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You won|Rock beats scissors.';
+    if (playerScore < 5) {
+      playerScore += 1;
+      document.getElementById('player').innerHTML = playerScore;
+    }
   }
   if (playerChoice === 'paper' && computerChoice === 'rock') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You won|Paper beats rock.';
+    if (playerScore < 5) playerScore += 1;
+    document.getElementById('player').innerHTML = playerScore;
   }
   if (playerChoice === 'scissors' && computerChoice === 'paper') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You won|Scissors beats paper.';
+    if (playerScore < 5) playerScore += 1;
+    document.getElementById('player').innerHTML = playerScore;
   }
   if (playerChoice === 'rock' && computerChoice === 'paper') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You lost|Paper beats rock.';
+    if (computerScore < 5) computerScore += 1;
+    document.getElementById('computer').innerHTML = computerScore;
   }
   if (playerChoice === 'paper' && computerChoice === 'scissors') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You lost|Scissors beats paper.';
+    if (computerScore < 5) computerScore += 1;
+    document.getElementById('computer').innerHTML = computerScore;
   }
   if (playerChoice === 'scissors' && computerChoice === 'rock') {
     let outCome = document.getElementById('message');
     outCome.textContent = 'You lost|Rock beats scissors.';
+    if (computerScore < 5) computerScore += 1;
+    document.getElementById('computer').innerHTML = computerScore;
   }
 }
